74ac1009bc4fa3d464f4efe1203a38bd53e9efe5
Jay Satiro <raysatiro@yahoo.com>
2017-07-19 18:04:16 -0400

    Improve SwitchToNotepadPlusPlusWindow
    - Check for the foreground window's root owner, and if it's Notepad++ do
      not attempt a switch.
    
    .. because it's possible Notepad++ may own a top-level enabled popup
    that's the foreground window (eg "File xxx doesn't exist, create it?")
    and which has disabled the main window. In such a case if Notepad++ is
    not minimized then the popup is already the foreground and visible.
    
    - In the steal focus hack explicitly call SW_RESTORE if
      SwitchToThisWindow can't restore the window.
    
    .. because it's possible that a disabled main window may not restore
    even if there are enabled popups.
    
    - Try harder to set the foreground window to remedy a race condition.
    
    .. because it's possible that if a previously existing mono-instance
    of Notepad++ has a disabled main window then the just-started Notepad++
    instance will try to set that as the foreground window (incorrectly in
    my view) and terminate, at the same time this program attempts to set
    the Notepad++ enabled popup as the foreground window.
    
    - Add some programming notes in 'npp NOTES.txt'.

--- npp/npp.cpp
+++ npp/npp.cpp
@@ -1,6 +1,6 @@
 /* LICENSE: MIT License
 Copyright (C) 2017 Jay Satiro <raysatiro@yahoo.com>
-https://github.com/jay/RunAsDesktopUser/blob/master/LICENSE
+https://github.com/jay/npp_launcher/blob/master/LICENSE
 */
 
 #include <windows.h>
@@ -27,74 +27,209 @@ using namespace std;
     wstringstream ss_d_; \
     ss_d_ << "npp launcher: " << streamargs << "\n"; \
     OutputDebugStringW(ss_d_.str().c_str()); \
-  } WHILE_FALSE;
+  } WHILE_FALSE
 #else
 #define DEBUGMSG(streamargs)
 #endif
 
+
+/*
+Attempt to set the foreground window for a short period of time.
+
+This function calls SetForegroundWindow one or more times for up to
+dwMilliseconds. Each call is separated by a small interval of sleep.
+
+Return TRUE (success) as soon as SetForegroundWindow returns TRUE; otherwise
+return FALSE when dwMilliseconds has passed. Behavior may be modified by the
+flags below.
+
+Flags
+-----
+
+SFW_CONFIRM:
+After a successful call to SetForegroundWindow do not consider it successful
+or return early unless GetForegroundWindow == hWnd.
+
+SFW_ENFORCE:
+Set and enforce hWnd as the foreground window by calling SetForegroundWindow
+when GetForegroundWindow != hwnd, for the entire dwMilliseconds.
+Return the result of the last GetForegroundWindow == hWnd.
+*/
+BOOL SetForegroundWindowTryHarder(HWND hWnd, DWORD dwMilliseconds,
+                                  DWORD dwFlags = 0)
+{
+  const DWORD interval = 100;
+
+#define SFW_CONFIRM  (1<<0)
+#define SFW_ENFORCE  (1<<1)
+
+  DWORD start = GetTickCount();
+  BOOL ret = 0;
+  DWORD gle = 0;
+
+  SetLastError(0);
+
+  for(DWORD elapsed = 0; elapsed <= dwMilliseconds;
+      elapsed = GetTickCount() - start) {
+    /* GetTickCount operates at the resolution of the system clock and it may
+       return the same result on subsequent calls. */
+    DWORD remaining = dwMilliseconds - elapsed;
+
+    if(!ret) {
+      ret = SetForegroundWindow(hWnd);
+      gle = GetLastError();
+    }
+
+    for(int i = 0; i < 2; ++i) {
+      if((dwFlags & (SFW_CONFIRM | SFW_ENFORCE)))
+        ret = (GetForegroundWindow() == hWnd);
+
+      if(ret && !(dwFlags & SFW_ENFORCE))
+        return TRUE;
+
+      if(!i)
+        Sleep(remaining < interval ? remaining : interval);
+    }
+  }
+
+  SetLastError(gle);
+  return ret;
+}
+
+/* Try to switch to Notepad++ for up to approximately dwMilliseconds + several
+   seconds. */
 void SwitchToNotepadPlusPlusWindow(void)
 {
+  const DWORD interval = 100;
+  const DWORD dwMilliseconds = 5000;
+
   VOID (WINAPI *SwitchToThisWindow)(HWND hWnd, BOOL fAltTab) =
     (VOID (WINAPI *)(HWND, BOOL))GetProcAddress(GetModuleHandleW(L"user32"),
                                                 "SwitchToThisWindow");
 
-  if(!SwitchToThisWindow)
-    return;
+  DWORD start = GetTickCount();
 
-  int interval = 100;
-  int max_wait = 5000;
+  for(DWORD elapsed = 0; elapsed <= dwMilliseconds;
+      elapsed = GetTickCount() - start) {
+    /* GetTickCount operates at the resolution of the system clock and it may
+       return the same result on subsequent calls. */
+    DWORD remaining = dwMilliseconds - elapsed;
 
-  /* Try to set Notepad++ as the foreground window */
-  for(int i = 0; i < (max_wait / interval); ++i) {
-    Sleep((DWORD)interval);
+    Sleep(remaining < interval ? remaining : interval);
 
     HWND hFore = GetForegroundWindow();
     if(!hFore)
       continue;
 
-    /* Get the class name of the foreground window. The received class name is
-       truncated if the buffer is not big enough, so to ensure we don't receive
-       a truncated class name that compares equal to classname the buffer must
-       be sized 1 more than expected for countof classname. */
+    HWND hForeRootOwner = GetAncestor(hFore, GA_ROOTOWNER);
+    if(!hForeRootOwner)
+      continue;
+
+    // Find the Notepad++ window as hwnd
+    HWND hwnd;
     const wchar_t classname[] = L"Notepad++";
+    /* The buffer that receives the class name must be 1 more than countof
+       classname to ensure that a truncated class name won't compare equal. */
     wchar_t buf[_countof(classname) + 1];
-    if(GetClassNameW(hFore, buf, (int)_countof(buf)) &&
-       !wcscmp(classname, buf))
+    if(GetClassNameW(hForeRootOwner, buf, (int)_countof(buf)) &&
+       !wcscmp(classname, buf)) {
+      // Notepad++ has the foreground so stop if it's enabled and not minimized
+      if(IsWindowEnabled(hFore) && !IsIconic(hFore) &&
+         !IsIconic(hForeRootOwner))
+        break;
+      hwnd = hForeRootOwner;
+    }
+    else {
+      hwnd = FindWindowW(classname, NULL);
+      if(!hwnd)
+        continue;
+
+      hwnd = GetAncestor(hwnd, GA_ROOTOWNER);
+      if(!hwnd)
+        continue;
+    }
+
+    DEBUGMSG("GetForegroundWindow: " << hFore <<
+             " (GA_ROOTOWNER: " << hForeRootOwner << ")");
+
+    HWND hwndPop = GetWindow(hwnd, GW_ENABLEDPOPUP);
+
+    DEBUGMSG("Notepad++ window: " << hwnd <<
+             " (GW_ENABLEDPOPUP: " << hwndPop << ")");
+
+    /* GW_ENABLEDPOPUP is documented to return hwnd if there's no popup, but
+       I've found it can return NULL even if hwnd is enabled. */
+    if(!hwndPop && IsWindowEnabled(hwnd))
+      hwndPop = hwnd;
+
+    /* Target an enabled Notepad++ window, if possible:
+       Some enabled popups such as the "Create it?" message box disable hwnd.
+       Since SetForegroundWindow can assign the focus to a disabled window we
+       want to avoid that if possible by targeting an enabled window, but if
+       not we'll use hwnd as a fallback: Notepad++ calls SetForegroundWindow on
+       its main window (hwnd) even if it's disabled. */
+    HWND hTarget = (IsWindowEnabled(hwnd) ? hwnd :
+                    (hwndPop && IsWindowEnabled(hwndPop)) ? hwndPop :
+                    hwnd);
+
+    DEBUGMSG("Target window: " << hTarget);
+
+    if(!hTarget)
       break;
 
-    HWND hwnd = FindWindowW(classname, NULL);
-    if(!hwnd)
-      continue;
+    DEBUGMSG("IsWindowEnabled: " <<
+             (IsWindowEnabled(hTarget) ? "TRUE" : "FALSE"));
 
-    Sleep(1);
-
-    // if the foreground window has changed and is Notepad++ then nothing to do
-    if(hwnd == GetForegroundWindow())
-      break;
-
-    // if the foreground window has changed then try again
-    if(hFore != GetForegroundWindow())
-      continue;
-
-    DEBUGMSG("GetForegroundWindow: " << hFore);
-    DEBUGMSG("Notepad++ window: " << hwnd);
-
-    // if the foreground window sets properly then nothing to do
-    if(SetForegroundWindow(hwnd)) {
-      DEBUGMSG("SetForegroundWindow: TRUE");
-      break;
+    /* Enforce hTarget as the foreground window for 300ms.
+       This is to remedy a race condition with Notepad++ where it may set the
+       disabled main window of a previously existing mono-instance to the
+       foreground while at the same time this program is trying to set the
+       foreground to the Notepad++ enabled popup that has disabled the main
+       window. */
+    if(SetForegroundWindowTryHarder(hTarget, 300, SFW_ENFORCE)) {
+      DEBUGMSG("SetForegroundWindowTryHarder SFW_ENFORCE: TRUE");
+      if(!IsIconic(hwnd) && !IsIconic(hTarget))
+        break;
     }
     else
-      DEBUGMSG("SetForegroundWindow: FALSE");
+      DEBUGMSG("SetForegroundWindowTryHarder SFW_ENFORCE: FALSE");
 
-    // steal focus: minimize and then restore in alt+tab style
+    // steal focus: minimize and then restore in alt+tab style.
     DEBUGMSG("Attempting to steal focus");
-    ShowWindow(hwnd, SW_MINIMIZE);
-    SwitchToThisWindow(hwnd, TRUE);
-    /* there's no way to know for sure whether that method is going to succeed.
-       the foreground window may be in transition due to above so it's possible
-       GetForegroundWindow returns 0. or it's possible it just doesn't work at
-       all. in any case to avoid flickering don't try it again. */
+
+    if(!IsIconic(hwnd)) {
+      BOOL b = ShowWindow(hwnd, SW_MINIMIZE);
+      DEBUGMSG("ShowWindow SW_MINIMIZE: " << (b ? "TRUE" : "FALSE"));
+    }
+
+    if(SwitchToThisWindow) {
+      SwitchToThisWindow(hwnd, TRUE);
+      DEBUGMSG("SwitchToThisWindow");
+    }
+
+    /* Restore the window if necessary. SwitchToThisWindow should have already
+       done this, however it's possible that it can't restore the window for
+       some reason. For example I observe it doesn't always restore if hwnd is
+       disabled (likely due to a popup), but SW_RESTORE does work. For details
+       refer to "npp NOTES.txt" */
+    if(!SwitchToThisWindow || IsIconic(hwnd)) {
+      BOOL b = ShowWindow(hwnd, SW_RESTORE);
+      DEBUGMSG("ShowWindow SW_RESTORE: " << (b ? "TRUE" : "FALSE"));
+    }
+
+    DEBUGMSG("IsIconic Notepad++ window: " <<
+             (IsIconic(hwnd)? "TRUE" : "FALSE"));
+    DEBUGMSG("IsIconic target window: " <<
+             (IsIconic(hTarget)? "TRUE" : "FALSE"));
+    DEBUGMSG("GetForegroundWindow: " << GetForegroundWindow());
+
+    // Try for up to 2 seconds to make hTarget the foreground window
+    if(SetForegroundWindowTryHarder(hTarget, 2000, SFW_CONFIRM))
+      DEBUGMSG("SetForegroundWindowTryHarder SFW_CONFIRM: TRUE");
+    else
+      DEBUGMSG("SetForegroundWindowTryHarder SFW_CONFIRM: FALSE");
+
+    // to avoid flickering don't try again
     break;
   }
 }
